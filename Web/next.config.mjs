c4a52ea7e5d6aeec3d9798921022099e34c608bb
MasRfif <odri.naufal@gmail.com>
2024-09-03 09:29:12 +0700

    adding next config

--- Web/next.config.mjs
+++ Web/next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.ctfassets.net",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.daisyui.com",
+      },
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
