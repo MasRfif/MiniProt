23747285afc0377000229b12e7c4696250ee16c2
MasRfif <odri.naufal@gmail.com>
2024-09-11 16:04:31 +0700

    adding Profile function 50%

---
+++ Web/src/app/(page)/admin/page.tsx
@@ -0,0 +1,7 @@
+export default function adminPg() {
+  return (
+    <>
+      <h1>aaaa</h1>
+    </>
+  );
+}
---
+++ Web/src/app/(page)/user/page.tsx
@@ -0,0 +1,15 @@
+export default function UserPage() {
+  return (
+    <>
+      <div className="user-profile">
+        <h2>First </h2>
+        <p>Username: aaa</p>
+      </div>
+      <div className="wallet-info">
+        <h3>Wallet Information</h3>
+        <p>Balance: a</p>
+        <p>Points: a</p>
+      </div>
+    </>
+  );
+}
