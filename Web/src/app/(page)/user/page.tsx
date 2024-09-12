583550f768f48126f29a40585039656147ba4a8e
MasRfif <odri.naufal@gmail.com>
2024-09-12 14:03:04 +0700

    adding user-page

--- Web/src/app/(page)/user/page.tsx
+++ Web/src/app/(page)/user/page.tsx
@@ -1,15 +1,18 @@
 export default function UserPage() {
   return (
     <>
-      <div className="user-profile">
-        <h2>First </h2>
-        <p>Username: aaa</p>
-      </div>
-      <div className="wallet-info">
-        <h3>Wallet Information</h3>
-        <p>Balance: a</p>
-        <p>Points: a</p>
-      </div>
+      <section className=" w-full h-screen flex  justify-center items-center font-bold  bg-gradient-to-b from-black to-red-700/90">
+        <div className="flex flex-row-reverse w-7/12 h-1/2 rounded-3xl bg-white">
+          <div className="w-full h-2 flex justify-center items-center">
+            <div className="avatar">
+              <div className="w-48 rounded-full">
+                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              </div>
+            </div>
+          </div>
+          <div>aaa</div>
+        </div>
+      </section>
     </>
   );
 }
