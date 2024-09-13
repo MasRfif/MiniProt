58b9961cfeeed545bb3497f4d2961454f2d09026
anchansp <andhikasp1701jp@gmail.com>
2024-09-13 10:24:20 +0700

    tes_login

--- Web/src/app/(page)/Login/page.tsx
+++ Web/src/app/(page)/Login/page.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState("");
   const router = useRouter();
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
