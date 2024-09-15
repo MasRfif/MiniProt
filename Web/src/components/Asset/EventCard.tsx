b5d78af461beab2c51deba84a21eb92ebfb178a3
anchansp <andhikasp1701jp@gmail.com>
2024-09-15 22:38:48 +0700

    added link in hero carousel

--- Web/src/components/Asset/EventCard.tsx
+++ Web/src/components/Asset/EventCard.tsx
@@ -28,31 +28,33 @@ export default function EventCard() {
     <>
       {/* break */}
       {(eventData as any)?.data?.map((e: any) => (
-        <div className="carousel-item " key={e.id}>
-          <div className="card bg-base-100 w-96 h-96 shadow-xl">
-            <figure className="h-48">
-              <Image
-                src={e.imageUrl || ""}
-                alt="Image of event venue"
-                width={600}
-                height={600}
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">
-                {e.eventName}
-                <div className="badge badge-secondary">
-                  {e.isPaid ? "BUY" : "FREE"}
+        <Link href={`/event/${e.id}`} key={e.id}>
+          <div className="carousel-item ">
+            <div className="card bg-base-100 w-96 h-96 shadow-xl">
+              <figure className="h-48">
+                <Image
+                  src={e.imageUrl || ""}
+                  alt="Image of event venue"
+                  width={600}
+                  height={600}
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">
+                  {e.eventName}
+                  <div className="badge badge-secondary">
+                    {e.isPaid ? `Rp.${e.price},00` : "FREE"}
+                  </div>
+                </h2>
+                <p className="text-sm">{e.description}</p>
+                <div className="card-actions justify-end">
+                  <div className="badge badge-outline">event tag</div>
+                  <div className="badge badge-outline">event tag</div>
                 </div>
-              </h2>
-              <p className="text-sm">{e.description}</p>
-              <div className="card-actions justify-end">
-                <div className="badge badge-outline">event Tag</div>
-                <div className="badge badge-outline">event Tag</div>
               </div>
             </div>
           </div>
-        </div>
+        </Link>
       ))}
       {/* break */}
     </>
